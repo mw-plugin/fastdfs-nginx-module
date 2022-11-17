2d16ea4a13fe02699a4d3bed036694fa6ee50f11
midoks <midoks@163.com>
2022-11-17 23:02:13 +0800

    Update installl.sh

--- installl.sh
+++ installl.sh
@@ -5,8 +5,10 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 mkdir -p /www/server/source
 cd /www/server/source
 
-git clone https://github.com/happyfish100/fastdfs-nginx-module
 
+if [ ! -d /www/server/source/fastdfs-nginx-module ];then
+	git clone https://github.com/happyfish100/fastdfs-nginx-module
+fi
 
 
 VERSION=1.21.4.1
