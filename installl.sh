adbee9d326e41034b4733503a0c46f402d3292a6
midoks <midoks@163.com>
2022-11-17 22:26:54 +0800

    Create installl.sh

---
+++ installl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+
+
+mkdir -p /www/server/source
+cd /www/server/source
+
+git clone https://github.com/mw-plugin/fastdfs-nginx-module
